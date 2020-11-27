20c3fd799e8e3b86a94c145e1d331e38b8a41a50
Jonathan Jacobson <jonjac@gmail.com>
2020-11-27 22:52:53 +0200

    sort output by freq

--- book-TheGoProgrammingLanguage/ch04/4.9/wordfreq.go
+++ book-TheGoProgrammingLanguage/ch04/4.9/wordfreq.go
@@ -3,19 +3,61 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
+	"sort"
 )
 
-func main() {
-	freqs := make(map[string]int)
+type wordToFrequencyMap map[string]int
+type frequencyToWordsMap map[int]string
 
-	input := bufio.NewScanner(os.Stdin)
-	input.Split(bufio.ScanWords)
-	for input.Scan() {
-		freqs[input.Text()]++
+func groupByFrequency(wf wordToFrequencyMap) frequencyToWordsMap {
+
+	freqWord := make(frequencyToWordsMap)
+
+	for w, f := range wf {
+		freqWord[f] = fmt.Sprintf("%s, %q", freqWord[f], w)
 	}
 
-	for k, v := range freqs {
-		fmt.Println(k, v)
+	return freqWord
+
+}
+
+func getKeysSorted(m frequencyToWordsMap) []int {
+	keys := make([]int, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+
+	sort.Ints(keys)
+
+	return keys
+}
+
+func countWords(r io.Reader) wordToFrequencyMap {
+	wordFreq := make(wordToFrequencyMap)
+
+	input := bufio.NewScanner(r)
+	input.Split(bufio.ScanWords)
+	for input.Scan() {
+		wordFreq[input.Text()]++
+	}
+
+	return wordFreq
+}
+
+func main() {
+	// Scan input and keep word counts in map
+	wordFreq := countWords(os.Stdin)
+
+	// Group word-count words by frequencies
+	freqWord := groupByFrequency(wordFreq)
+
+	// Order frequencies
+	frequencies := getKeysSorted(freqWord)
+
+	// Print frequency-to-word-list map contents ordered by frequencies
+	for _, f := range frequencies {
+		fmt.Printf("%10d: %s\n", f, freqWord[f])
 	}
 }
